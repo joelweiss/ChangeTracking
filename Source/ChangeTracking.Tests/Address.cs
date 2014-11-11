ffc1d066dd5ee4c71cb6fe18c031511151fe1cd9
Joel Weiss <2joelweiss@gmail.com>
2014-11-11 01:03:46 -0500

    Added: Missing files.

---
+++ Source/ChangeTracking.Tests/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeTracking.Tests
+{
+    public class Address
+    {
+        public virtual int AddressId { get; set; }
+        public virtual string City { get; set; }
+    }
+}
---
+++ Source/ChangeTracking.Tests/OrderDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeTracking.Tests
+{
+    public class OrderDetail
+    {
+        public virtual int OrderDetailId { get; set; }
+        public virtual string ItemNo { get; set; }
+    }
+}
