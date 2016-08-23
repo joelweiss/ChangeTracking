895530e64be79c875e70560fcc3379fe09ea8d5d
Solomon Sabel <solsab@alldatahealth.com>
2016-08-23 13:15:48 -0400

    Skip properties with no setters on GetOriginal

---
+++ Source/ChangeTracking.Tests/OrderWithCalculatedFields.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeTracking.Tests
+{
+    public class OrderWithCalculatedFields
+    {
+        public virtual int Id { get; set; }
+        public virtual string CustomerNumber { get; set; }
+        public virtual Address Address { get; set; }
+        public virtual IList<OrderDetail> OrderDetails { get; set; }
+        public virtual string FormattedId
+        {
+            get { return CustomerNumber != null ? CustomerNumber + "-" + Id.ToString() : null; }
+        }
+
+        public Order CreateOrder()
+        {
+            return new Order();
+        }
+    }
+}
