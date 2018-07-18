9f5123d9d97dcbb301bb4cb9e1d1cca7064b0acd
Joel Weiss <joelweiss@outlook.com>
2018-07-18 18:06:10 -0400

    Fixed: Virtual Methods Issue #36

--- Source/ChangeTracking/ChangeTrackingProxyGenerationHook.cs
+++ Source/ChangeTracking/ChangeTrackingProxyGenerationHook.cs
@@ -1,6 +1,7 @@
 ﻿using Castle.DynamicProxy;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace ChangeTracking
@@ -8,27 +9,36 @@ namespace ChangeTracking
     public class ChangeTrackingProxyGenerationHook : IProxyGenerationHook
     {
         private static HashSet<string> _MethodsToSkip;
+        private readonly Type _Type;
+        private HashSet<MethodInfo> _InstanceMethodsOnClass;
 
         static ChangeTrackingProxyGenerationHook()
         {
             _MethodsToSkip = new HashSet<string> { "Equals", "GetType", "ToString", "GetHashCode" };
         }
 
+        public ChangeTrackingProxyGenerationHook(Type type)
+        {
+            _Type = type;
+            _InstanceMethodsOnClass = new HashSet<MethodInfo>(type
+                .GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly)
+                .Where(mi => !mi.IsSpecialName));
+        }
+
         public void MethodsInspected() { }
 
         public void NonProxyableMemberNotification(Type type, MemberInfo memberInfo)
         {
-            var method = memberInfo as MethodInfo;
-            if (method != null && method.IsProperty())
+            if (memberInfo is MethodInfo methodInfo && methodInfo.IsProperty())
             {
-                throw new InvalidOperationException(string.Format("Property {0} is not virtual. Can't track classes with non-virtual properties.", method.Name.Substring("set_".Length)));
+                throw new InvalidOperationException($"Property {methodInfo.Name.Substring("set_".Length)} is not virtual. Can't track classes with non-virtual properties.");
             }
         }
 
-        public bool ShouldInterceptMethod(Type type, System.Reflection.MethodInfo methodInfo) => !_MethodsToSkip.Contains(methodInfo.Name);
+        public bool ShouldInterceptMethod(Type type, System.Reflection.MethodInfo methodInfo) => !_MethodsToSkip.Contains(methodInfo.Name) && !_InstanceMethodsOnClass.Contains(methodInfo);
 
-        public override bool Equals(object obj) => obj as ChangeTrackingProxyGenerationHook != null;
+        public override bool Equals(object obj) => (obj as ChangeTrackingProxyGenerationHook)?._Type == _Type;
 
-        public override int GetHashCode() => 0;
+        public override int GetHashCode() => _Type.GetHashCode();
     }
 }
