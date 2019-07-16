4c287dbb01a95d4a3ced264ebf3001d3a0f4b102
Joel Weiss <joelweiss@outlook.com>
2019-07-16 18:42:42 -0400

    Added: Failing Test

---
+++ Source/ChangeTracking.Tests/GraphTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ChangeTracking.Internal;
+using FluentAssertions;
+using Xunit;
+
+namespace ChangeTracking.Tests
+{
+    public class GraphTests
+    {
+        [Fact]
+        public async Task Add_To_Graph_OnOther_Thread_Should_Not_Throw()
+        {
+            Graph graph = new Graph();
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+            _ = Task.Run(() =>
+            {
+                int i = 0;
+                while (!cancellationTokenSource.IsCancellationRequested)
+                {
+                    graph.Add(new ProxyWeakTargetMap(i, i));
+                    i++;
+                }
+            });
+
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    graph.Invoking(g => g.GetExistingProxyForTarget(int.MaxValue)).Should().NotThrow<InvalidOperationException>();
+                    await Task.Delay(i);
+                }
+            }
+            finally
+            {
+                cancellationTokenSource.Cancel();
+            }
+        }
+    }
+}
--- Source/ChangeTracking/Properties/AssemblyInfo.cs
+++ Source/ChangeTracking/Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: InternalsVisibleTo("ChangeTracking.Tests")]
