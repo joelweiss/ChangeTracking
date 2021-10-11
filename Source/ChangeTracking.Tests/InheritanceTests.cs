2a6f659cee30967134f86c4785a7cd5e1c0fe70e
Vladimir Gaytanov (Developer) <vgdev@crystalint.onmicrosoft.com>
2021-10-10 23:49:35 -0400

    Tests used to verify that actual type of tracking proxy passed into collection corresponds that of created proxy.

---
+++ Source/ChangeTracking.Tests/InheritanceTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ChangeTracking.Tests
+{
+    public class InheritanceTests
+
+    {
+        public class Container
+        {
+            public virtual IList<Item> Items { get; set; } = new List<Item>();
+
+            public void AddItem(Item item)
+            {
+                Items.Add(item);
+            }
+        }
+
+        public  class Item
+        {
+            public Item()
+            {                
+            }
+
+            public virtual int ValueInt { get; set; }
+        }
+
+        public class ItemDerived : Item
+        {
+            public ItemDerived() :
+                base()
+            {
+
+            }
+        }
+
+        [Fact]
+        public void ItemsAdd_DerivedCollectionItemClass_AddsItemOfDerivedCollectionItemClass()
+        {
+            Container container = new Container();
+
+            var trackable = container.AsTrackable();
+
+            trackable.Items.Add(new ItemDerived());
+
+            trackable.CastToIChangeTrackable().AcceptChanges();
+
+            Assert.True(container.Items[0] is ItemDerived);
+        }
+
+    }
+}
